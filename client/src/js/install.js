4e93f57d3a75a45871db66d348bb951f6e2a8b3b
kaylab78 <kaylabackus78@gmail.com>
2022-08-28 16:08:45 -0500

    Worked with boot camp tutor Meg Meyers to fix the error I was getting in the console. I need to clean up the code.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,15 +3,31 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event - Done
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
+    // event.preventDefault();
+    // Defer prompt into later in the page lifecycle. (tutor)
+    window.deferredPrompt = event;
     butInstall.style.visibility = 'visible';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element - Done
+// butInstall.addEventListener('click', async () => {
+//     // event.prompt();
+//     butInstall.setAttribute('disabled', true);
+//     butInstall.textContent = "Installed!";
+// });
+
 butInstall.addEventListener('click', async () => {
-    // event.prompt();
-    butInstall.setAttribute('disabled', true);
-    butInstall.textContent = "Installed!";
+    const promptEvent = window.deferredPrompt;
+  
+    if (!promptEvent) {
+      return;
+    }
+  
+    promptEvent.prompt();
+  
+    window.deferredPrompt = null;
+  
+    butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event - Done
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,52 +42,92 @@ module.exports = () => {
         swDest: 'service-worker.js'
       }),
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: 'Just Another Text Editor',
-        short_name: 'JATE',
-        description: 'An easy to use text editor.',
-        background_color: '#000',
-        theme_color: '#31a9e1',
-        start_url: './',
-        publicPath: './',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
-          {
-            src: path.resolve('src/images/logo.png'),
-            size: '1024x1024',
-            destination: path.join('assets', 'icons'),
-            purpose: 'maskable'
-          }
         ],
-      })
+      }),
     ],
+      // new WebpackPwaManifest({
+      //   name: 'Just Another Text Editor',
+      //   short_name: 'JATE',
+      //   description: 'An easy to use text editor.',
+      //   background_color: '#000',
+      //   theme_color: '#31a9e1',
+      //   start_url: './',
+      //   publicPath: './',
+      //   icons: [
+      //     {
+      //       src: path.resolve('src/images/logo.png'),
+      //       sizes: [96, 128, 192, 256, 384, 512],
+      //       destination: path.join('assets', 'icons'),
+      //     },
+      //     {
+      //       src: path.resolve('src/images/logo.png'),
+      //       size: '1024x1024',
+      //       destination: path.join('assets', 'icons'),
+      //       purpose: 'maskable'
+      //     }
+      //   ],
+      // })
+    // ],
 
-    module: {
-      rules: [
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', { targets: "defaults" }]
-              ]
-            }
-          }
-        }
-      ],
+//     module: {
+//       rules: [
+//         {
+//           test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//           type: 'asset/resource',
+//         },
+//         {
+//           test: /\.css$/i,
+//           use: ['style-loader', 'css-loader'],
+//         },
+//         {
+//           test: /\.m?js$/,
+//           exclude: /node_modules/,
+//           use: {
+//             loader: 'babel-loader',
+//             options: {
+//               presets: [
+//                 ['@babel/preset-env', { targets: "defaults" }]
+//               ]
+//             }
+//           }
+//         }
+//       ],
+//     },
+//   };
+// };
+module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
     },
-  };
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+        },
+      },
+    },
+  ],
+},
+};
 };
